e39c09b87eadab008908f97ed8591f34c98908b1
Petter Blomberg <git@petterblomberg.se>
2015-12-27 20:30:52 +0100

    add reactor status

--- powerpanel.cs
+++ powerpanel.cs
@@ -7,6 +7,7 @@ void Main(string argument)
     text.Add("Updated: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
     text.Add("-----------------");
     text.AddRange(Solar_status());
+    text.AddRange(Reactor_status());
 
     UpdateLCDs(String.Join("\n", text.ToArray()));
 }
@@ -57,6 +58,52 @@ List<String> Solar_status() {
     return text;
 }
 
+List<String> Reactor_status() {
+    // IMyFunctionalBlock has the Enabled property, which the IMyTerminalBlock doesn't
+    List<IMyFunctionalBlock> reactors = new List<IMyFunctionalBlock>();
+    GridTerminalSystem.GetBlocksOfType<IMyReactor>(reactors);
+    String info = "";
+
+    System.Text.RegularExpressions.Regex outputRegex = new System.Text.RegularExpressions.Regex(
+        "Max Output: (\\d+\\.?\\d*) (\\w?)W.*Current Output: (\\d+\\.?\\d*) (\\w?)W",
+        System.Text.RegularExpressions.RegexOptions.Singleline);
+
+    double total_max = 0.0f;
+    double total_current = 0.0f;
+    int reactors_on = 0;
+    int reactors_off = 0;
+
+    for(int i = 0;i<reactors.Count;i++) {
+        if(reactors[i].Enabled) {
+            reactors_on++;
+
+            info = reactors[i].DetailedInfo;
+            double currentOutput = 0.0f;
+            double maxOutput = 0.0f;
+            double parsedDouble;
+            System.Text.RegularExpressions.Match match = outputRegex.Match(info);
+            if(match.Success) {
+                if(Double.TryParse(match.Groups[1].Value, out parsedDouble)) {
+                    maxOutput = parsedDouble * Math.Pow(1000.0, MULTIPLIERS.IndexOf(match.Groups[2].Value));
+                }
+                if(Double.TryParse(match.Groups[3].Value, out parsedDouble)) {
+                    currentOutput = parsedDouble * Math.Pow(1000.0, MULTIPLIERS.IndexOf(match.Groups[4].Value));
+                }
+            }
+            total_max += maxOutput;
+            total_current += currentOutput;
+        } else {
+            reactors_off++;
+        }
+    }
+
+    List<String> text = new List<String>();
+    text.Add("Reactors: " + reactors_on + " on, " + reactors_off + " off");
+    text.Add("Output: " + Format(total_current) + "W  (max: " + Format(total_max) + "W)");
+    text.Add("-----------------");
+    return text;
+}
+
 void UpdateLCDs(String s) {
     // Find LCDs and update them
     List<IMyTerminalBlock> lcds = new List<IMyTerminalBlock>();
@@ -79,3 +126,5 @@ String Format(double power) {
     }
     return "" + Math.Round(power,2).ToString("##0.00") + " " + MULTIPLIERS.Substring(count,1);
 }
+
+// vim: tabstop=4 shiftwidth=4 expandtab
